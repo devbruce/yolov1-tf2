0643922f227e0261381aad0421161483a743d271
DevBruce <bruce93k@gmail.com>
2021-03-23 00:12:59 +0900

    Update loss function

--- libs/losses.py
+++ libs/losses.py
@@ -11,7 +11,7 @@ def get_losses(one_pred, one_label, cfg):
     """
     Args:
       one_pred (EagerTensor dtype=tf.float32): 3-D Tensor shape=[cell_size, cell_size, boxes_per_cell*5 + num_classes]
-      one_label (EagerTensor dtype=tf.float32): 2-D Tensor shape=[obj_n, 5], 5 --> [x_center_rel_in_cell, y_center_rel_in_cell, width_rel, height_rel, class_index] (Relative coordinate)
+      one_label (EagerTensor dtype=tf.float32): 2-D Tensor shape=[obj_n, 5], 5 --> [x_center_in_cell, y_center_in_cell, width, height, class_index] (Absolute coordinates)
       cfg: YOLO config object
 
     Returns:
@@ -23,8 +23,12 @@ def get_losses(one_pred, one_label, cfg):
     pred_confidences = pred_boxes_with_confidence[:, :, :, 4]
     pred_cell_classes = one_pred[:, :, cfg.boxes_per_cell*5:]
     
-    pred_cx_in_cell, pred_cy_in_cell = pred_boxes[:, :, :, 0], pred_boxes[:, :, :, 1]
-    pred_w_sqrt, pred_h_sqrt = pred_boxes[:, :, :, 2], pred_boxes[:, :, :, 3]
+    pred_cx_in_cell = pred_boxes[:, :, :, 0]
+    pred_cy_in_cell = pred_boxes[:, :, :, 1]
+    pred_w_sqrt = pred_boxes[:, :, :, 2]
+    pred_h_sqrt = pred_boxes[:, :, :, 3]
+    cell_w = cfg.input_width // cfg.cell_size
+    cell_h = cfg.input_height // cfg.cell_size
 
     # Loop the number of objects
     losses = {
@@ -34,7 +38,6 @@ def get_losses(one_pred, one_label, cfg):
         'noobj_loss': 0.,
         'class_loss': 0.,
     }
-    cell_w, cell_h = cfg.input_width // cfg.cell_size, cfg.input_height // cfg.cell_size
     obj_n = len(one_label)
     for obj_idx in range(obj_n):
         label = one_label[obj_idx]
@@ -59,8 +62,8 @@ def get_losses(one_pred, one_label, cfg):
         #===========#===========#===========#===========#===========#===========#===========#===========#===========
         
         # Resonsible box mask (Max IoU per cell)
-        pred_ltrb_abs = postprocess_yolo_format(pred_boxes_with_confidence, cfg.input_height, cfg.input_width, cfg.cell_size, cfg.boxes_per_cell)
-        ious = calc_iou(pred_ltrb_abs, label[:-1], cfg)
+        pred_ltrb = postprocess_yolo_format(pred_boxes_with_confidence, cfg.input_height, cfg.input_width, cfg.cell_size, cfg.boxes_per_cell)
+        ious = calc_iou(pred_ltrb, label[:-1], cfg)
 
         max_ious = tf.reduce_max(ious, axis=2, keepdims=True)  # shape = [cell_size, cell_size, 1]
         resonsible_box_mask = tf.cast(tf.greater_equal(ious, max_ious), tf.float32)  # shape = [cell_size, cell_size, boxes_per_cell]
@@ -132,49 +135,3 @@ def train_step(model, optimizer, batch_imgs, batch_labels, cfg):
     batch_losses['noobj_loss'] /= len(batch_imgs)
     batch_losses['class_loss'] /= len(batch_imgs)
     return batch_losses
-
-
-def _pred_limit_zero2one(pred_tensor):
-    pred_np = pred_tensor.numpy()
-    over_one_mask = np.ones_like(pred_np, dtype=np.float32)
-    over_one_mask[pred_np > 1.] = 0.
-    tf_over_one_mask = tf.convert_to_tensor(over_one_mask, dtype=tf.float32)
-    pred_tensor = pred_tensor * tf_over_one_mask
-
-    add_one_mat = np.zeros_like(pred_np)
-    add_one_mat[np.where(pred_tensor.numpy() == 0.)] = 1.
-    pred_tensor = pred_tensor + add_one_mat
-
-    neg_mask = np.ones_like(pred_np, dtype=np.float32)
-    neg_mask[pred_np < 0.] = 0.
-    tf_neg_mask = tf.convert_to_tensor(neg_mask, dtype=tf.float32)
-
-    pred_tensor = pred_tensor * neg_mask
-    return pred_tensor
-
-
-def _get_neg2neg_mask(pred):
-    pred_np = pred.numpy()
-    neg_idxs = np.where(pred_np < 0.)
-    neg_mask = np.ones_like(pred_np)
-    neg_mask[neg_idxs] *= -1
-    neg_mask = tf.convert_to_tensor(neg_mask, dtype=tf.float32)
-    return neg_mask
-
-
-def _get_neg2zero_mask(pred):
-    pred_np = pred.numpy()
-    idxs = np.where(pred_np < 0.)
-    mask = np.ones_like(pred_np)
-    mask[idxs] *= 0.
-    mask = tf.convert_to_tensor(mask, dtype=tf.float32)
-    return mask
-
-
-def _get_pos2zero_mask(pred):
-    pred_np = pred.numpy()
-    idxs = np.where(pred_np >= 0.)
-    mask = np.ones_like(pred_np)
-    mask[idxs] *= 0.
-    mask = tf.convert_to_tensor(mask, dtype=tf.float32)
-    return mask
