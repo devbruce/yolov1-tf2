0b4408d7707230a5a685c88ad256b925744a7384
DevBruce <bruce93k@gmail.com>
2021-03-25 10:41:26 +0900

    Convert gt box abs to rel of calc_iou

--- libs/iou.py
+++ libs/iou.py
@@ -9,9 +9,8 @@ def calc_iou(pred_boxes, gt_box, cfg):
     """Calculate ious between pred_boxes and gt_box
     Args:
       pred_boxes (EagerTensor dtype=tf.float32): 4-D tensor [cell_size, cell_size, boxes_per_cell, 5] ==> [x_min, y_min, x_max, y_max, confidence] (Absolute coordinates)
-      gt_box (EagerTensor dtype=tf.float32): 1-D tensor [4] ==> [x_center, y_center, width, height] (Absolute coordinates)
+      gt_box (EagerTensor dtype=tf.float32): 1-D tensor [4] ==> [x_center_rel, y_center_rel, width_rel, height_rel]
       cfg: YOLO config object
-
     Returns:
       iou (EagerTensor dtype=tf.float32): 3-D tensor [cell_size, cell_size, boxes_per_cell]
     """
@@ -20,7 +19,11 @@ def calc_iou(pred_boxes, gt_box, cfg):
     pred_w = pred_xmax - pred_xmin
     pred_h = pred_ymax - pred_ymin
 
-    gt_cx, gt_cy, gt_w, gt_h = gt_box[0], gt_box[1], gt_box[2], gt_box[3]
+    gt_cx_rel, gt_cy_rel, gt_w_rel, gt_h_rel = gt_box[0], gt_box[1], gt_box[2], gt_box[3]
+    gt_cx = gt_cx_rel * cfg.input_width
+    gt_cy = gt_cy_rel * cfg.input_height
+    gt_w = gt_w_rel * cfg.input_width
+    gt_h = gt_h_rel * cfg.input_height
     gt_xmin, gt_ymin = gt_cx - (gt_w / 2), gt_cy - (gt_h / 2)
     gt_xmax, gt_ymax = gt_cx + (gt_w / 2), gt_cy + (gt_h / 2)
 
@@ -42,4 +45,3 @@ def calc_iou(pred_boxes, gt_box, cfg):
     union = (pred_area + gt_area) - inter_area + cfg.eps
     ious = inter_area / union
     return ious
-    
